945e2bbb57cafebeb637f747e6d0770447bf1da3
amigin <amigin@gmail.com>
2024-11-25 14:59:08 +0200

    Added data_state

---
+++ src/data_state.rs
@@ -0,0 +1,50 @@
+use std::fmt::Debug;
+
+#[derive(Debug)]
+pub enum DataState<T: Debug> {
+    None,
+    Loading,
+    Loaded(T),
+    Error(String),
+}
+
+impl<T: Debug> DataState<T> {
+    pub fn is_none(&self) -> bool {
+        match self {
+            DataState::None => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_loading(&self) -> bool {
+        match self {
+            DataState::Loading => true,
+            _ => false,
+        }
+    }
+
+    pub fn to_not_loaded_cases(&self) -> Option<NotLoadedCases> {
+        match self {
+            DataState::None => NotLoadedCases::None.into(),
+            DataState::Loading => NotLoadedCases::Loading.into(),
+            DataState::Loaded(_) => None,
+            DataState::Error(_) => None,
+        }
+    }
+
+    pub fn as_ref(&self) -> &Self {
+        self
+    }
+}
+
+impl<T: Debug> From<T> for DataState<T> {
+    fn from(value: T) -> Self {
+        Self::Loaded(value)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum NotLoadedCases {
+    None,
+    Loading,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
 pub mod js;
 pub extern crate js_sys;
 pub extern crate web_sys;
+mod data_state;
+pub use data_state::*;
