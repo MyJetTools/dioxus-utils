a82104b3d03c10defa35642b4079b96f8cf0dc65
amigin <amigin@gmail.com>
2025-10-08 22:28:55 +0300

    Fixes

--- src/console_log.rs
+++ src/console_log.rs
@@ -1,10 +1,11 @@
 #[cfg(not(feature = "server"))]
 pub fn console_log(message: &str) {
+    let escaped_message = escape_for_java_script_string(message);
     let js = format!(
         r#"
         console.log('{}');
     "#,
-        message
+        escaped_message
     );
 
     crate::eval(&js);
@@ -13,4 +14,22 @@ pub fn console_log(message: &str) {
 #[cfg(feature = "server")]
 pub fn console_log(message: &str) {
     println!("{}", message);
+}
+
+fn escape_for_java_script_string(message: &str) -> String {
+    let mut result = String::with_capacity(message.len());
+
+    for ch in message.chars() {
+        match ch {
+            '\\' => result.push_str("\\\\"),
+            '\'' => result.push_str("\\'"),
+            '"' => result.push_str("\\\""),
+            '\n' => result.push_str("\\n"),
+            '\r' => result.push_str("\\r"),
+            '\t' => result.push_str("\\t"),
+            _ => result.push(ch),
+        }
+    }
+
+    result
 }
